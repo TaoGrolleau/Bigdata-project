36d650520d563fdc72455cf7ca6e8e8797bb3421
Bissuelquentin <qbissuel@gmail.com>
2020-01-23 16:46:34 +0100

    Merging

--- src/Main.py
+++ src/Main.py
@@ -2,28 +2,27 @@ import Create_Data_Files as cdf
 import Parser_Article as p
 import PdfExtractor as pe
 import Data_Analyser as d
-import TextRankKeywords as trk
 import TopicModeling as tm
 
 articles = p.cleaning_articles()
-#cdf.create_csv_file_for_R(articles)
+cdf.create_csv_file_for_R(articles)
 
 pdf_files = dict()
 
 i = 0
-for elem in articles[:1]:
+for elem in articles:
     print(i, elem)
-    #pe.download_pdf_files(elem.pdfarticle, i)
+    pe.download_pdf_files(elem.pdfarticle, i)
     i += 1
 
-# d.create_wordcloud(articles)
-# d.create_graph_authors(articles)
+d.create_wordcloud(articles)
+d.create_graph_authors(articles)
+
 tm.return_main_topics()
-#d.create_wordcloud(articles)
-#d.create_graph_authors(articles)
+
 
 references_dic = pe.get_references(pe.get_authors_list(articles))
 #Plot the most famous authors
 pe.make_plot_famous_authors(references_dic, 50)
 
-#cdf.create_authors_graph_file(articles, 2)
+cdf.create_authors_graph_file(articles, 2)
